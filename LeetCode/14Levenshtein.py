f10451f2621d5b53dd36e9d574a02c2cc642c32f
Mr.Map <18201788952@163.com>
2018-04-18 23:04:05 +0800

    _levenshtein_distance

--- LeetCode/14Levenshtein.py
+++ LeetCode/14Levenshtein.py
@@ -1,43 +1,27 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import numpy as np
 
 
+# 编辑距离
+def _levenshtein_distance(input_x, input_y):
+    xlen = len(input_x) + 1  # 此处需要多开辟一个元素存储最后一轮的计算结果
+    ylen = len(input_y) + 1
 
-def minEditDist(sm, sn):
-    m, n = len(sm) + 1, len(sn) + 1
+    dp = np.zeros(shape=(xlen, ylen), dtype=int)
+    for i in range(0, xlen):
+        dp[i][0] = i
+    for j in range(0, ylen):
+        dp[0][j] = j
 
-    # create a matrix (m*n)
-
-    matrix = [[0] * n for i in range(m)]
-
-    matrix[0][0] = 0
-    for i in range(1, m):
-        matrix[i][0] = matrix[i - 1][0] + 1
-
-    for j in range(1, n):
-        matrix[0][j] = matrix[0][j - 1] + 1
-
-    for i in range(m):
-        print(matrix[i])
-
-    print("********************")
-
-    cost = 0
-
-    for i in range(1, m):
-        for j in range(1, n):
-            if sm[i - 1] == sn[j - 1]:
-                cost = 0
+    for i in range(1, xlen):
+        for j in range(1, ylen):
+            if input_x[i - 1] == input_y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
             else:
-                cost = 1
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+    return dp[xlen - 1][ylen - 1]
 
-            matrix[i][j] = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost)
-
-    for i in range(m):
-        print(matrix[i])
-
-    return matrix[m - 1][n - 1]
-
-mindist = minEditDist("ivan1", "ivan2")
+mindist = _levenshtein_distance("ivan1", "ivan2")
 print(mindist)
